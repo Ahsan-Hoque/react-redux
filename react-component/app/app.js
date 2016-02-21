6b081b1cfdfbf312546b30fb29e3d487e18b7479
ahsan-info <ahsan.ror@gmail.com>
2016-02-21 21:36:22 +0600

    react component hello world

---
+++ react-component/app/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import FirstComponent from './firstcomponent.js'
+
+ReactDom.render((
+	< FirstComponent/>
+	),document.getElementById('root'));
---
+++ react-component/app/firstcomponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const FirstComponent = React.createClass({
+	render: function(){
+		return(
+			<div> Hello World</div>
+			)
+	}
+});
+
+export default FirstComponent;
